1ab15e63ce9d186d08a6f9c79e964d636f70b3db
FrameTee29 <teeraphat123@hotmail.com>
2020-03-12 12:55:12 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BearList from './components/StudentList'
+
+function App() {
+  return (
+    <div>
+      <h2>Students</h2>
+      <BearList />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/StudentList.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import './StudentList.css';
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+
+const StudentList = props =>{
+    return(
+        <div>
+            bear
+        </div>
+    );
+}
+
+export default StudentList;
