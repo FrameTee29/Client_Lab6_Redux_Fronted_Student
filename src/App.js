e9504e5f34d0a068caf11627af4ef5e69646a869
FrameTee29 <teeraphat123@hotmail.com>
2020-03-12 14:24:24 +0700

    clear App

--- src/App.js
+++ src/App.js
@@ -7,55 +7,11 @@ import InputForm from './components/InputForm'
 
 function App() {
 
-  const [students, setStudents] = useState({})
-  const [formData, setData] = useState({
-    sid: 0,
-    firstname: '',
-    lastname: '',
-    weight: 0,
-    height: 0,
-    level: '',
-    img: '',
-  });
-
-  const { sid, firstname, lastname, weight, height, level, img } = formData;
-
-  const getStudents = async () => {
-    const result = await axios.get(`http://localhost/api/students`)
-    setStudents(result.data)
-  }
-
-  const addStudent = async () => {
-    const result = await axios.post(`http://localhost/api/students/`, {
-      sid, firstname, lastname, weight, height, level, img
-    })
-    console.log(result.data)
-    getStudents()
-  }
-
-  const deleteStudent = async (id) => {
-    const result = await axios.delete(`http://localhost/api/students/${id}`)
-    console.log(result.data)
-    getStudents()
-  }
-
-  const updateStudent = async (id) => {
-    console.log(sid,firstname,lastname,weight,height,level,img);
-    const result = await axios.put(`http://localhost/api/students/${id}`, {
-      sid,firstname,lastname,weight,height,level,img
-    })
-    console.log('Student id update: ', result.data)
-    getStudents()
-  }
-
-  useEffect(() => {
-    getStudents()
-  }, [])
 
   return (
     <div>
-      <StudentList  deleteStudent={deleteStudent} updateStudent={updateStudent}/>
-      <InputForm data={formData} onChange={setData} addStudent={addStudent}/>
+      <StudentList   />
+      <InputForm />
     </div>
 
   );
--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -4,7 +4,19 @@ import { useSelector, useDispatch } from 'react-redux'
 import './StudentCard.css';
 
 const StudentCard = props => {
-    console.log("props show" + JSON.stringify(props))
+   
+    const form = useSelector(state => state.form)
+    const dispatch = useDispatch();
+
+    const deleteStudent = async (id) => {
+        await axios.delete(`http://localhost/api/students/${props.id}`)
+        dispatch({ type: 'DELETE_STUDENT', sid: props.id })
+      }
+
+      const updateStudent = async () => {
+        await axios.put(`http://localhost/api/students/${props.id}`, form)
+        dispatch({ type: 'UPDATE_STUDENT', sid:props.id , students: {...form,sid:props.id} })
+    }
     return (
         <div>
             <div className='bearcard-container'>
@@ -16,8 +28,8 @@ const StudentCard = props => {
                     <p className='bearcard-name'>{props.firstname} {props.lastname}</p>
                 </div>
                 <div className='bearcard-actions'>
-                    <div onClick={props.updateStudent}>Update</div>
-                    <div onClick={props.deleteStudent}>Delete</div>
+                    <div onClick={updateStudent}>Update</div>
+                    <div onClick={deleteStudent}>Delete</div>
                 </div>
             </div>
         </div>
